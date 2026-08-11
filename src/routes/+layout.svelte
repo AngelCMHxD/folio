4a0b0ca259d9071f39bb8ca6742861279e9d59ca
Angel <57822483+AngelCMHxD@users.noreply.github.com>
2026-08-11 00:15:53 -0500

    change favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,7 +4,7 @@ import { page } from "$app/state";
 import type { Pathname } from "$app/types";
 import { locales, localizeHref } from "$lib/paraglide/runtime";
 import "./layout.css";
-import favicon from "$lib/assets/favicon.svg";
+import favicon from "$lib/assets/favicon.png";
 
 let { children } = $props();
 </script>
