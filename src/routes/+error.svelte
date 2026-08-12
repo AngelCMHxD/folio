b4f7f381c21be47435c507290fa0b03cb28c7fce
Angel <57822483+AngelCMHxD@users.noreply.github.com>
2026-08-12 11:17:37 -0500

    add 404 page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,29 @@
+<script>
+import { goto } from "$app/navigation";
+import { page } from "$app/state";
+import { m } from "$lib/paraglide/messages";
+</script>
+
+<main class="flex flex-col items-center justify-center p-6 text-center">
+	<div>
+		<p class="text-sm font-semibold text-ctp-blue uppercase">
+			Error {page.status}
+		</p>
+
+		<h1 class="text-3xl font-bold tracking-tight sm:text-4xl">
+			{m.not_found()}
+		</h1>
+
+		<div class="mt-6">
+			<button
+				type="button"
+				tabindex="-1"
+				class="w-fit"
+				onclick={() => goto('/')}
+			>
+				{"$"}
+				cd ~
+			</button>
+		</div>
+	</div>
+</main>
