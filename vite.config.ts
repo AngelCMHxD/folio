375d05332462dbf745d75d71d6b1b23aa9e19797
Angel <57822483+AngelCMHxD@users.noreply.github.com>
2026-08-12 11:35:47 -0500

    setup SSG

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { paraglideVitePlugin } from "@inlang/paraglide-js";
-import adapter from "@sveltejs/adapter-auto";
+import adapter from "@sveltejs/adapter-static";
 import { sveltekit } from "@sveltejs/kit/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
@@ -16,9 +16,6 @@ export default defineConfig({
 						: true,
 			},
 
-			// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-			// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-			// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 			adapter: adapter(),
 		}),
 
